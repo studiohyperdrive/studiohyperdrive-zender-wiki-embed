e0f663ca3725f5046137fcc5194671604c3ad62f
Milat Omed <milat.omed@studiohyperdrive.be>
2022-07-26 18:22:02 +0200

    fix: make standalone lit work

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import { createReadStream } from 'fs';
 import { createServer } from 'http';
+import url from 'url';
 
 const server = createServer((req, res) => {
+	let path = url.parse(req.url, true).pathname;
+	// // '/folder/to/file/' becomes 'folder/to/file'
+	path = path.replace(/^\/+|\/+$/g, '');
+
+	if (path === 'src/my-element.ts') {
+		res.writeHead(200, { 'content-type': 'application/javascript' });
+		createReadStream('../dist/zender-wikipedia-embed_web_lit_2022.js').pipe(res);
+		return;
+	}
+
 	res.writeHead(200, { 'content-type': 'text/html' });
 	createReadStream('../index.html').pipe(res);
 });

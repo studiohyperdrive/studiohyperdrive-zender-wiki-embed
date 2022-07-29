2e1b73b3f02cf96b9357457fa4202325b472e83a
Milat Omed <milat.omed@studiohyperdrive.be>
2022-07-29 15:42:27 +0200

    refactor: make setup compatible with toolforge

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
+/*
+	This file is used for ToolForge hosting.
+*/
+
+/* eslint-disable no-undef */
 import { createReadStream } from 'fs';
 import { createServer } from 'http';
 import url from 'url';
+
+const port = parseInt(process.env.PORT, 10) || 3000;
 
 const server = createServer((req, res) => {
 	let path = url.parse(req.url, true).pathname;
@@ -9,12 +16,12 @@ const server = createServer((req, res) => {
 
 	if (path === 'src/my-element.ts') {
 		res.writeHead(200, { 'content-type': 'application/javascript' });
-		createReadStream('../dist/zender-wikipedia-embed_web_lit_2022.js').pipe(res);
+		createReadStream('dist/zender-wikipedia-embed_web_lit_2022.js').pipe(res);
 		return;
 	}
 
 	res.writeHead(200, { 'content-type': 'text/html' });
-	createReadStream('../index.html').pipe(res);
+	createReadStream('index.html').pipe(res);
 });
 
-server.listen(3000);
+server.listen(port, () => console.log(`Wiki embed app listening at port ${port}`));
